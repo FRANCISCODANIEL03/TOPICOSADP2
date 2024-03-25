38bc31e49f684a152c483fe5e4bcd9991c8a5b99
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-24 21:56:42 -0600

    Update

--- Login_Form/js/cuenta.js
+++ Login_Form/js/cuenta.js
@@ -1,10 +1,10 @@
-let nonmbre = document.getElementById("nombre")
-nonmbre.innerHTML = localStorage.getItem("nombre");
-let imagen = document.getElementById("img2");
-imagen.src = localStorage.getItem("imagen");
-const cal =()=>{
-    window.location.href="/html/calculadora.html";
+let nonmbre = document.getElementById("nombre");//se crea el evento del h2 "nombre"
+nonmbre.innerHTML = localStorage.getItem("nombre");//se extrae el valor anteriormente guardado y se inserta en el evento del h2 
+let imagen = document.getElementById("img2");//se crea el evento de la img "img2"
+imagen.src = localStorage.getItem("imagen");//se extrae la imagen guardada y se inserta en el evento creado
+const cal =()=>{//se crea la funcion "cal" para abrir la calculadora
+    window.location.href="/Login_Form/html/calculadora.html";//se ingresa a la pagina de la calculadora
 }
-const cance3 =()=>{
-    window.location.href="/html/index.html";
+const cance3 =()=>{//se crea la funcion "cance3" para cerrar sesion
+    window.location.href="/Login_Form/html/index.html";//se regresa a la pagina de inicio
 }
